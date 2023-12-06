eaa8e8be804dd31979a555fbad8bd424f4aecfe1
Kirsten Comandich <kcomandich@gmail.com>
2023-12-06 10:08:01 -0800

    Add spec for numbering photos from the same city sequentially

--- spec/photo-org_spec.rb
+++ spec/photo-org_spec.rb
@@ -2,11 +2,20 @@ require_relative '../photo-org.rb'
 
 RSpec.describe PhotoOrg do
   describe "solution" do
-    let(:sample) { "photo.jpg, Krakow, 2013-09-05 14:08:15\nMike.png, London, 2015-06-20 15:13:22" }
-    let(:p)      { PhotoOrg.new }
+    let(:sample) do
+      "photo.jpg, Krakow, 2013-09-05 14:08:15
+        Mike.png, London, 2015-06-20 15:13:22
+        myFriends.png, Krakow, 2013-09-05 14:07:13"
+    end
+    let(:p) { PhotoOrg.new }
 
     it "should return a list the same length as the input" do
-      expect(p.solution(sample).count).to eq 2
+      expect(p.solution(sample).count).to eq 3
+    end
+
+    it "should number two photos from the same city sequentially by date" do
+      expect(p.solution(sample)[0]).to eq "Krakow2.jpg"
+      expect(p.solution(sample)[2]).to eq "Krakow1.png"
     end
   end
 end
